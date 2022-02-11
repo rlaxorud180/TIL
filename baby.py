428f36c2b9c9cd89cf63fe4f46af9e272f888ff7
rlaxorud180 <rlaxorud814@gmail.com>
2022-02-11 21:32:42 +0900

    0211

---
+++ baby.py
@@ -0,0 +1,55 @@
+def baby_gin(a):
+    
+    count=0
+    for i in a:
+        if a.count(i)==3 or a.count(i)==6:
+            a.remove(i)
+            a.remove(i)
+            a.remove(i)
+            count=count+1
+
+
+    if count==2:
+        return 1
+   
+    flag=0
+    
+    if len(a)==3:
+        for i in a[0:2]:
+       
+            if i+1 in a:
+                flag=1
+            else:
+                flag=0
+                break  
+    
+    if len(a)==6:
+        a=set(a)
+        
+        a=list(a)
+            
+        for i in a[0:len(a)-1]:
+            if i+1 in a:
+                
+                flag=2
+            else:
+                flag=0
+                break
+        
+
+    if flag==1:
+        count=count+1
+    elif flag==2:
+        count=2
+    else:
+        count=0
+            
+    
+            
+    if count==2:
+        return 1
+    else:
+        return 0
+
+print(baby_gin([1,2,1,3,1,5]))
+#테스트케이스 다 돌아가긴 한데 너무 더럽... ㅠ
